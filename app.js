4cd19287d60574f42b3bacf29bb68abd608d2264
Matthew <mateicristiansandu21@gmail.com>
2021-10-31 11:51:35 +0200

    final changes

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const nextButton = document.querySelector(".next-button");
-
 //svg mazes
 const levelOne = document.querySelector(".level-one");
 const levelTwo = document.querySelector(".level-two");
-
-// UI MESSAGES
+//UI MESSAGES
 const uiLevel = document.querySelector(".ui-level");
 const uiMessage = document.querySelector(".ui-message");
+//End Game
+const spookyPicture = document.querySelector(".spooky-picture");
+const screamSound = document.querySelector(".scream-sound");
 
 const collisionCheck = (value) => {
-  if (value === "maze-border") {
-    alert("You lost...try again!");
-  }
-
+  if (value === "maze-border") alert("You lost...try again.");
   if (value === "finish") {
     nextButton.style.opacity = 1;
     nextButton.style.pointerEvents = "all";
     levelOne.style.pointerEvents = "none";
   }
-
   if (value === "end-game") {
+    screamSound.play();
+    spookyPicture.style.display = "block";
+    document.body.style.background = "black";
   }
 };
 
