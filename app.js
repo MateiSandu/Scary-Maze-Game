eea80e587abfd6a83824110efd8f0b093bc196cb
Matthew <mateicristiansandu21@gmail.com>
2021-10-31 11:43:29 +0200

    lots of changes

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+const nextButton = document.querySelector(".next-button");
+
+//svg mazes
+const levelOne = document.querySelector(".level-one");
+const levelTwo = document.querySelector(".level-two");
+
+// UI MESSAGES
+const uiLevel = document.querySelector(".ui-level");
+const uiMessage = document.querySelector(".ui-message");
+
+const collisionCheck = (value) => {
+  if (value === "maze-border") {
+    alert("You lost...try again!");
+  }
+
+  if (value === "finish") {
+    nextButton.style.opacity = 1;
+    nextButton.style.pointerEvents = "all";
+    levelOne.style.pointerEvents = "none";
+  }
+
+  if (value === "end-game") {
+  }
+};
+
 window.addEventListener("mousemove", (e) => {
-  console.log(e.target.classList.value);
+  let check = e.target.classList.value;
+  collisionCheck(check);
+});
+
+nextButton.addEventListener("click", () => {
+  levelOne.style.display = "none";
+  levelTwo.style.display = "block";
+  nextButton.style.opacity = 0;
+  nextButton.style.pointerEvents = "none";
+  uiLevel.textContent = "Level 2";
+  uiMessage.textContent = "One more to go...";
 });
